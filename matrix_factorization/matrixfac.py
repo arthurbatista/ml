ee870483ddd4b8dc6d655f282fb428fe200a108d
Arthur Batista <arthurbat@gmail.com>
2014-02-03 00:20:18 -0400

    add social regularization

--- matrix_factorization/matrixfac.py
+++ matrix_factorization/matrixfac.py
@@ -6,6 +6,9 @@
 #
 try:
     import numpy
+    import math
+    import scipy
+    from scipy.stats import pearsonr
 except:
     print "This implementation requires the numpy module."
     exit(0)
@@ -30,10 +33,10 @@ def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
         for i in xrange(len(R)):
             for j in xrange(len(R[i])):
                 if R[i][j] > 0:
-                    eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                    y = R[i][j] - numpy.dot(P[i,:],Q[:,j])
                     for k in xrange(K):
-                        P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
-                        Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
+                        P[i][k] = P[i][k] + alpha * (2 * y * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * y * P[i][k] - beta * Q[k][j])
         eR = numpy.dot(P,Q)
         e = 0
         for i in xrange(len(R)):
@@ -45,6 +48,86 @@ def matrix_factorization(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
         if e < 0.001:
             break
     return P, Q.T
+
+def matrix_factorization_2(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    Q = Q.T
+    for step in xrange(steps):
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    y = R[i][j] - numpy.dot(P[i,:],Q[:,j])
+                    for k in xrange(K):
+                        P[i][k] = P[i][k] + alpha * (2 * y * Q[k][j] - beta * P[i][k])
+                        Q[k][j] = Q[k][j] + alpha * (2 * y * P[i][k] - beta * Q[k][j])
+    return P, Q.T
+
+def matrix_factorization_lr(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    for step in xrange(steps):
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    theta_t = Q.T[:,j]
+                    x = P[i,:]
+                    r_aprox =  numpy.dot(x,theta_t) - R[i][j]
+                    for k in xrange(K):
+                        P[i][k] = x[k]       - alpha * (r_aprox * theta_t[k] + beta * x[k])
+                        Q[j][k] = theta_t[k] - alpha * (r_aprox * x[k]       + beta * theta_t[k])
+    return P, Q
+
+def matrix_factorization_lr_sr(R, P, Q, K, steps=5000, alpha=0.0002, beta=0.02):
+    for step in xrange(steps):
+        for i in xrange(len(R)):
+            for j in xrange(len(R[i])):
+                if R[i][j] > 0:
+                    theta_t = Q.T[:,j]
+                    x = P[i,:]
+                    r_aprox =  numpy.dot(x,theta_t) - R[i][j]
+                    for k in xrange(K):
+                        P[i][k] = x[k]       - alpha * (r_aprox * theta_t[k] + beta * x[k] + social_regularization(j,R,Q,k))
+                        Q[j][k] = theta_t[k] - alpha * (r_aprox * x[k]       + beta * theta_t[k] + social_regularization(j,R,Q,k))
+    return P, Q
+
+def social_regularization(u, R, Q, k, beta=0.02):
+    N = [
+         [0,0,0,1],
+         [0,0,0,1],
+         [1,1,0,1],
+         [1,0,0,0]
+        ]
+
+    N = numpy.array(N)
+
+    reg = 0.0
+
+    for i in xrange(len(N)):
+        if u != i:
+            if N[i][u] == 1:
+                reg += pearson_def(R[:,u], R[:,i])*(Q[u,k]-Q[i,k])
+
+    return beta*reg
+
+def average(x):
+    assert len(x) > 0
+    return float(sum(x)) / len(x)
+
+def pearson_def(x, y):
+    assert len(x) == len(y)
+    n = len(x)
+    assert n > 0
+    avg_x = average(x)
+    avg_y = average(y)
+    diffprod = 0
+    xdiff2 = 0
+    ydiff2 = 0
+    for idx in range(n):
+        xdiff = x[idx] - avg_x
+        ydiff = y[idx] - avg_y
+        diffprod += xdiff * ydiff
+        xdiff2 += xdiff * xdiff
+        ydiff2 += ydiff * ydiff
+
+    return diffprod / math.sqrt(xdiff2 * ydiff2)
+
 
 ###############################################################################
 
@@ -66,8 +149,30 @@ if __name__ == "__main__":
     P = numpy.random.rand(N,K)
     Q = numpy.random.rand(M,K)
 
+    P1 = numpy.copy(P);
+    Q1 = numpy.copy(Q);
+    
+    P2 = numpy.copy(P);
+    Q2 = numpy.copy(Q);
+
+    P3 = numpy.copy(P);
+    Q3 = numpy.copy(Q);
+    
     nP, nQ = matrix_factorization(R, P, Q, K)
-
     nR = numpy.dot(nP, nQ.T)
-
     print nR
+
+    nP1, nQ1 = matrix_factorization_2(R, P1, Q1, K)
+    nR1 = numpy.dot(nP1, nQ1.T)
+    print nR - nR1
+
+    nP2, nQ2 = matrix_factorization_lr(R, P2, Q2, K)
+    nR2 = numpy.dot(nP2, nQ2.T)
+    print nR - nR2
+
+    nP3, nQ3 = matrix_factorization_lr_sr(R, P3, Q3, K)
+    nR3 = numpy.dot(nP3, nQ3.T)
+    print nR - nR3
+
+    # reg = social_regularization(0,R,Q,0)
+    # print reg
